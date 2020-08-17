a2935cab01103aa0c61a159884d9fbdc0b916624
Pietro Colangelo <pietro.colangelo@sourcesense.com>
2020-08-17 10:07:05 +0200

    refactoring

--- src/index.js
+++ src/index.js
@@ -2,101 +2,99 @@ import "./css/style.css";
 import $ from "jquery";
 import { getConfig } from "./js/api";
 
-$(document).ready(function () {
-  getConfig().then((json) => populateMenu(json.menu));
+getConfig().then((json) => populateMenu(json.menu));
 
-  function populateMenu(menu) {
-    const lista = document.getElementById("lista");
-    for (let index = 0; index < menu.length; index++) {
-      if (menu[index].sub) {
-        const li_sub = document.createElement("li");
-        const ul_sub = document.createElement("ul");
-        const a_sub_title = document.createElement("a");
-        const linkText_sub_title = document.createTextNode(menu[index].label);
-        a_sub_title.appendChild(linkText_sub_title);
-        a_sub_title.href = "#";
-        $(li_sub).addClass("item-level-1");
-        $(ul_sub).addClass("sub");
-        ul_sub.id = menu[index].label.toLowerCase();
+function populateMenu(menu) {
+  const lista = document.getElementById("lista");
+  for (let index = 0; index < menu.length; index++) {
+    if (menu[index].sub) {
+      const li_sub = document.createElement("li");
+      const ul_sub = document.createElement("ul");
+      const a_sub_title = document.createElement("a");
+      const linkText_sub_title = document.createTextNode(menu[index].label);
+      a_sub_title.appendChild(linkText_sub_title);
+      a_sub_title.href = "#";
+      $(li_sub).addClass("item-level-1");
+      $(ul_sub).addClass("sub");
+      ul_sub.id = menu[index].label.toLowerCase();
 
-        //Title and Fontawesome Icon Append
-        const span = document.createElement("span");
-        const h3 = document.createElement("h3");
-        const i = document.createElement("i");
-        const titleSection = document.createTextNode(menu[index].label);
+      //Title and Fontawesome Icon Append
+      const span = document.createElement("span");
+      const h3 = document.createElement("h3");
+      const i = document.createElement("i");
+      const titleSection = document.createTextNode(menu[index].label);
 
-        h3.appendChild(titleSection);
-        span.appendChild(i);
+      h3.appendChild(titleSection);
+      span.appendChild(i);
 
-        $(span).addClass("return-level-1");
-        $(i).addClass("fa fa-angle-left");
+      $(span).addClass("return-level-1");
+      $(i).addClass("fa fa-angle-left");
 
-        ul_sub.appendChild(span);
-        ul_sub.appendChild(h3);
+      ul_sub.appendChild(span);
+      ul_sub.appendChild(h3);
 
-        for (const obj of menu[index].sub) {
-          const label_sub = obj.label;
-          const label_sub_title = obj.sub_title;
-          const url_sub = obj.url;
-          const li_sub_sub = document.createElement("li");
-          const a_sub = document.createElement("a");
-          const h4 = document.createElement("h4");
-          const p = document.createElement("p");
-          const linkText_sub_h4 = document.createTextNode(label_sub);
-          const linkText_sub_p = document.createTextNode(label_sub_title);
-          h4.appendChild(linkText_sub_h4);
-          p.appendChild(linkText_sub_p);
-          a_sub.appendChild(h4);
-          a_sub.appendChild(p);
+      for (const obj of menu[index].sub) {
+        const label_sub = obj.label;
+        const label_sub_title = obj.sub_title;
+        const url_sub = obj.url;
+        const li_sub_sub = document.createElement("li");
+        const a_sub = document.createElement("a");
+        const h4 = document.createElement("h4");
+        const p = document.createElement("p");
+        const linkText_sub_h4 = document.createTextNode(label_sub);
+        const linkText_sub_p = document.createTextNode(label_sub_title);
+        h4.appendChild(linkText_sub_h4);
+        p.appendChild(linkText_sub_p);
+        a_sub.appendChild(h4);
+        a_sub.appendChild(p);
 
-          a_sub.href = url_sub;
-          li_sub_sub.appendChild(a_sub);
+        a_sub.href = url_sub;
+        li_sub_sub.appendChild(a_sub);
 
-          ul_sub.appendChild(li_sub_sub);
+        ul_sub.appendChild(li_sub_sub);
 
-          li_sub.appendChild(a_sub_title);
-          li_sub.appendChild(ul_sub);
-        }
-        lista.appendChild(li_sub);
-      } else {
-        const label = menu[index].label;
-        const url = menu[index].url;
-        const li = document.createElement("li");
-        const a = document.createElement("a");
-        const linkText = document.createTextNode(label);
-        a.appendChild(linkText);
-        a.href = url;
-        li.appendChild(a);
-        lista.appendChild(li);
+        li_sub.appendChild(a_sub_title);
+        li_sub.appendChild(ul_sub);
       }
+      lista.appendChild(li_sub);
+    } else {
+      const label = menu[index].label;
+      const url = menu[index].url;
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      const linkText = document.createTextNode(label);
+      a.appendChild(linkText);
+      a.href = url;
+      li.appendChild(a);
+      lista.appendChild(li);
     }
   }
+}
 
-  $(".menu-btn").click(() => {
-    $(".overlay").fadeToggle(200);
-  });
-  $(".remove-icon-mobile").click(() => {
-    $(this).siblings().find(".move").removeClass("move");
-    $(".overlay").fadeToggle(200);
-  });
+$(".menu-btn").click(() => {
+  $(".overlay").fadeToggle(200);
+});
+$(".remove-icon-mobile").click(() => {
+  $(this).siblings().find(".move").removeClass("move");
+  $(".overlay").fadeToggle(200);
+});
 
-  $("#lista").on("click", ".item-level-1 a", function () {
-    console.log($(this));
-    const servizi = $("#servizi");
-    const soluzioni = $("#soluzioni");
-    var value = $(this).text().toLowerCase();
-    if (value === "servizi") {
-      servizi.show();
-      soluzioni.hide();
-    }
-    if (value === "soluzioni") {
-      soluzioni.show();
-      servizi.hide();
-    }
-    $(this).parent().parent().toggleClass("move");
-  });
+$("#lista").on("click", ".item-level-1 a", function () {
+  console.log($(this));
+  const servizi = $("#servizi");
+  const soluzioni = $("#soluzioni");
+  var value = $(this).text().toLowerCase();
+  if (value === "servizi") {
+    servizi.show();
+    soluzioni.hide();
+  }
+  if (value === "soluzioni") {
+    soluzioni.show();
+    servizi.hide();
+  }
+  $(this).parent().parent().toggleClass("move");
+});
 
-  $("#lista").on("click", ".sub .return-level-1 i", function () {
-    $(this).parent().parent().parent().parent().removeClass("move");
-  });
+$("#lista").on("click", ".sub .return-level-1 i", function () {
+  $(this).parent().parent().parent().parent().removeClass("move");
 });
--- src/js/overlay.js
+++ src/js/overlay.js
@@ -1,21 +1,16 @@
-$(document).ready(function() {
-  $(".menu-btn").click(function() {
+$(document).ready(function () {
+  $(".menu-btn").click(function () {
     $(".overlay").fadeToggle(200);
   });
-  $(".remove-icon-mobile").click(function() {
-    $(this)
-      .siblings()
-      .find(".move")
-      .removeClass("move");
+  $(".remove-icon-mobile").click(function () {
+    $(this).siblings().find(".move").removeClass("move");
     $(".overlay").fadeToggle(200);
   });
 
-  $(".menu_mobile .item-level-1 a").on("click", function() {
+  $(".menu_mobile .item-level-1 a").on("click", function () {
     const servizi = $("#servizi");
     const soluzioni = $("#soluzioni");
-    var value = $(this)
-      .text()
-      .toLowerCase();
+    var value = $(this).text().toLowerCase();
     if (value === "servizi") {
       servizi.show();
       soluzioni.hide();
@@ -24,21 +19,147 @@ $(document).ready(function() {
       soluzioni.show();
       servizi.hide();
     }
-    $(this)
-      .parent()
-      .parent()
-      .toggleClass("move");
+    $(this).parent().parent().toggleClass("move");
   });
 
   $(".menu_mobile .item-level-1 .sub .return-level-1 a").on(
     "click",
-    function() {
-      $(this)
-        .parent()
-        .parent()
-        .parent()
-        .parent()
-        .removeClass("move");
+    function () {
+      $(this).parent().parent().parent().parent().removeClass("move");
     }
   );
 });
+
+/*
+--------------------OLD HTML---------------------------
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Menu Overlay</title>
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+    <header>
+      <nav class="navbar">
+        <div class="container">
+          <a class="navbar-brand" href="#"> </a>
+
+          <span class="navbar-text">
+            <div class="menu-btn">
+              <span><img src="assets/imgs/menu.svg" /></span>
+            </div>
+          </span>
+        </div>
+      </nav>
+    </header>
+    <div class="overlay">
+      <div class="remove-icon-mobile">
+        <img src="assets/imgs/close.svg" alt="image"/>
+      </div>
+      <div class="menu_mobile">
+        <div class="container_mobile">
+          <ul>
+            <li>
+              <ul id="lista">
+                <li><a href="/chi-siamo-2">Chi siamo</a></li>
+                <li class="item-level-1">
+                  <a href="#">Soluzioni</a>
+                  <ul class="sub" id="soluzioni">
+                    <span class="return-level-1"
+                      ><a href="#"><i class="fa fa-angle-left"></i></a
+                    ></span>
+                    <h3>Soluzioni</h3>
+                    <li>
+                      <a href="/soluzioni/video">
+                        <h4>Video</h4>
+                        <p>Gestisci i tuoi video</p>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/soluzioni/immagini">
+                        <h4>Immagini</h4>
+                        <p>Gestisci le tue immagini</p>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/soluzioni/gallery">
+                        <h4>Gallerie</h4>
+                        <p>Gestisci le tue Gallery</p>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/soluzioni/siti-web">
+                        <h4>Siti Web</h4>
+                        <p>Crea i tuoi siti web</p>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/soluzioni/applicazioni">
+                        <h4>Applicazioni</h4>
+                        <p>Arricchisci l'offerta digitale</p>
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li class="item-level-1">
+                  <a href="#">Servizi</a>
+                  <ul class="sub" id="servizi">
+                    <span class="return-level-1">
+                      <a href="#">
+                        <i class="fa fa-angle-left"></i>
+                      </a>
+                    </span>
+                    <h3>Servizi</h3>
+                    <li>
+                      <a href="/servizi/comunicazione">
+                        <h4>Comunicazione</h4>
+                        <p>Lorem ipsum dolor sit amet dor</p>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/servizi/fruizione">
+                        <h4>Fruizione</h4>
+                        <p>Lorem ipsum dolor sit amet dor</p>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/servizi/formazione-e-consulenza">
+                        <h4>Formazione e consulenza</h4>
+                        <p>Lorem ipsum dolor sit amet dor</p>
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li><a href="/i-nostri-clienti">I nostri clienti</a></li>
+                <li><a href="/lavora-con-noi">Lavora con noi</a></li>
+                <li><a href="/contatti">Contatti</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <script
+      src="https://code.jquery.com/jquery-3.1.0.js"
+      integrity="sha256-slogkvB1K3VOkzAI8QITxV3VzpOnkeNVsKvtkYLMjfk="
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
+
+*/
